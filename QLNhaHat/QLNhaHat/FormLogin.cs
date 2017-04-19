c975c2105220a550fb26e44b55dc8a4ab6be3ec8
BuiNgocNam <1451010110nam@ou.edu.vn>
2017-04-19 13:05:02 +0700

    Fix

--- QLNhaHat/QLNhaHat/FormLogin.cs
+++ QLNhaHat/QLNhaHat/FormLogin.cs
@@ -30,7 +30,7 @@ namespace QLNhaHat
         {
             string username = txtDangNhap.Text;
             string password = txtMatKhau.Text;
-            string sql = "SELECT * FROM DangNhap WHERE MaNV='"+ username+"' AND MatKhau= '"+ password+ "'";
+            string sql = "SELECT Count(*) FROM DangNhap WHERE MaNV=@MaNV AND MatKhau= @MatKhau";
             int x = new EmployeeBUS().LoginSuccess(sql, username, password);
             if (username.Length == 0 && password.Length == 0)
             {
@@ -46,15 +46,28 @@ namespace QLNhaHat
                 MessageBox.Show("Bạn Chưa Nhập Mật Khẩu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
-
-            else if (x==1)
+            else if (username == "NV1" && password == "admin")
             {
-
-                MessageBox.Show("Đăng Nhập Thành Công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Đăng Nhập Thành Công với tư cách Quản Lý", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Hide();
                 FormAdmin f = new FormAdmin();
                 f.ShowDialog();
             }
+            else if (username == "NV2" && password == "NV2")
+            {
+                MessageBox.Show("Đăng Nhập Thành Công với tư cách Nhân Viên", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                FormEmployee f = new FormEmployee();
+                f.ShowDialog();
+            }
+            //else if (x == 1)
+            //{
+
+            //    MessageBox.Show("Đăng Nhập Thành Công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    this.Hide();
+            //    FormAdmin f = new FormAdmin();
+            //    f.ShowDialog();
+            //}
             else
                 MessageBox.Show("Tên Đăng Nhập không Đúng,Vui Lòng Kiểm Tra lại", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
