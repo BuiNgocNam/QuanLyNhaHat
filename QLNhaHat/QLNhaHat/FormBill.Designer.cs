cebd0aa124d50e7500c168c4b0c07b47e96828d0
ChuThanhPhuong <1451010137phuong@ou.edu.vn>
2017-04-19 15:47:36 +0700

    Form Hoa Don

---
+++ QLNhaHat/QLNhaHat/FormBill.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace QLNhaHat
+{
+    partial class FormBill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMaVe = new System.Windows.Forms.Label();
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.lblMaNV = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTongTien = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::QLNhaHat.Properties.Resources.theme;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 329);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(89, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã Vé:";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(89, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã Khách Hàng:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(89, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mã Nhân Viên:";
+            // 
+            // lblMaVe
+            // 
+            this.lblMaVe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblMaVe.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMaVe.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaVe.ForeColor = System.Drawing.Color.Black;
+            this.lblMaVe.Location = new System.Drawing.Point(268, 71);
+            this.lblMaVe.Name = "lblMaVe";
+            this.lblMaVe.Size = new System.Drawing.Size(308, 23);
+            this.lblMaVe.TabIndex = 1;
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblMaKH.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMaKH.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaKH.ForeColor = System.Drawing.Color.Black;
+            this.lblMaKH.Location = new System.Drawing.Point(268, 172);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(308, 23);
+            this.lblMaKH.TabIndex = 1;
+            // 
+            // lblMaNV
+            // 
+            this.lblMaNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblMaNV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMaNV.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaNV.ForeColor = System.Drawing.Color.Black;
+            this.lblMaNV.Location = new System.Drawing.Point(268, 122);
+            this.lblMaNV.Name = "lblMaNV";
+            this.lblMaNV.Size = new System.Drawing.Size(308, 23);
+            this.lblMaNV.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(89, 226);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 23);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Tổng tiền:";
+            // 
+            // lblTongTien
+            // 
+            this.lblTongTien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblTongTien.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTongTien.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongTien.ForeColor = System.Drawing.Color.Black;
+            this.lblTongTien.Location = new System.Drawing.Point(268, 225);
+            this.lblTongTien.Name = "lblTongTien";
+            this.lblTongTien.Size = new System.Drawing.Size(308, 23);
+            this.lblTongTien.TabIndex = 1;
+            // 
+            // FormBill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 329);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMaNV);
+            this.Controls.Add(this.lblMaKH);
+            this.Controls.Add(this.lblTongTien);
+            this.Controls.Add(this.lblMaVe);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormBill";
+            this.Text = "Hóa Đơn";
+            this.Load += new System.EventHandler(this.FormBill_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMaVe;
+        private System.Windows.Forms.Label lblMaKH;
+        private System.Windows.Forms.Label lblMaNV;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTongTien;
+    }
+}
---
+++ QLNhaHat/QLNhaHat/FormBill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLNhaHat
+{
+    public partial class FormBill : Form
+    {
+        public FormBill(string MaVe,string MaNV, string MaKH, int TongTien)
+        {
+            InitializeComponent();
+            lblMaVe.Text = MaVe;
+            lblMaNV.Text = MaNV;
+            lblMaKH.Text = MaKH;
+            lblTongTien.Text = TongTien.ToString();
+        }
+
+        private void FormBill_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
